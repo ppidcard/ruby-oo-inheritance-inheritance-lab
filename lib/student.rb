b4931fe5552f641cb564fce8b022cb223c2a90a1
ppidcard <tvxqlikei@live.cn>
2021-07-21 07:43:45 +1000

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+    def initialize
+        self.knowledge = []
+    end
 
+    def learn(new_knowledge)
+        self.knowledge << new_knowledge
+    end
 end
